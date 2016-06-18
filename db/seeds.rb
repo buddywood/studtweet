00dd1fa717eba88b0e137cb0def86468e31c8194
Billy Talton <ebillytalton@yahoo.com>
2016-06-17 20:18:08 -0500

    seeds conference update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Conference.create!(name: 'American Athletic Conference', yrFounded: 1979, totalMembers: 11, headquarters: 'Providence, RI')
+Conference.create!(name: 'Atlantic Coast Conference', yrFounded: 1953, totalMembers: 15, headquarters: 'Greensboro, NC'))
+Conference.create!(name: 'Big Ten Conference', yrFounded: 1896, totalMembers: 14, headquarters: 'Rosemont, IL')
+Conference.create!(name: 'Big 12 Conference', yrFounded: 1996, totalMembers: 10, headquarters: 'Irving, TX')
+Conference.create!(name: 'Conference USA', yrFounded: 1995, totalMembers: 14, headquarters: 'Irving, TX')
+Conference.create!(name: 'Division I FBS Independents', yrFounded: 0, totalMembers: 3, headquarters: 'None')
+Conference.create!(name: 'Mid-American Conference', yrFounded: 1946, totalMembers: 12, headquarters: 'Cleveland, OH')
+Conference.create!(name: 'Mountain West Conference', yrFounded: 1999, totalMembers: 11, headquarters: 'Colorado Springs, CO'))
+Conference.create!(name: 'Pac-12 Conference', yrFounded: 1959, totalMembers: 12, headquarters: 'Walnut Creek, CA'))
+Conference.create!(name: 'Southeastern Conference', yrFounded: 1932, totalMembers: 14, headquarters: 'Birmingham, AL'))
+Conference.create!(name: 'Sun Belt Conference', yrFounded: 1976\, totalMembers: 11, headquarters: 'New Orleans, LA'))
